bd13e7203ac11d842630fd63340dd52c516aa9e6
ertan3 <ernesttan96@gmail.com>
2023-05-05 15:14:44 -0700

    created our first view file

--- ninety-nine-cats/db/seeds.rb
+++ ninety-nine-cats/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do
+    puts "destroying all tables..."
+    Cat.destroy_all
+
+    puts "reseting id sequence..."
+    ApplicationRecord.connection.reset_pk_sequence!("cats")
+
+    puts "creating seed data..."
+    cat1 = Cat.create!(birth_date: "2010/05/05", color: "orange", name: "Garfield", sex: "M", description: "i am garfield and I am lazy")
+    cat2 = Cat.create!(birth_date: "2016/06/05", color: "green", name: "Bubba", sex: "F", description: "meow meow meow")
+
+
+    puts "seeding complete"
+end
